0d6751dc15327272117268c014b7d9607d99530c
Diego Diaz <berith.py@gmail.com>
2020-10-17 15:45:31 -0300

    eslint working on the backend

--- trx-backend/README.md
+++ trx-backend/README.md
@@ -1,1 +1,57 @@
-# Requirements 
+# Willdom backend challenge
+
+Small nodejs webserver written with koa+sequelize which serves data from a postgresql db.
+
+## Requirements
+
+- node 12+
+- docker
+
+# Commands
+
+## Unix like
+
+For Mac and Linux theres a makefile that I like to use as a makeshift launcher for the whole thing. It provides a couple of utilities to make it easier to launch the project.
+
+### Run
+
+`$ make run`  
+The quickest way to launch the server is to run which launches the postgres container, run the creation migration and seeding of sequelize and also runs the server.
+
+### Stop
+
+`$ make stop`  
+Theres also a stop command to stop the postgres docker component
+
+### Test
+
+`$ npm run test`
+
+## Other os
+
+### Setup
+
+`$ npm install`
+
+### Linting
+
+`$ npm run lint`
+
+### Run
+
+`$ docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres`  
+`$ cd db; npx sequelize db:create`  
+`$ cd db; npx sequelize db:migrate`  
+`$ cd db; npx sequelize db:seed:all`  
+First we need to run the postgresql db, then we create the db, run the migrations and then we seed the user table.  
+After that you can start the server.  
+`$ npm start`
+
+### Stop
+
+`$ docker stop pg-docker`
+
+# TODO
+
+- Add some testing and mocking
+- Add husky to lint on every commit
--- trx-backend/src/index.js
+++ trx-backend/src/index.js
@@ -3,7 +3,6 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
 const Models = require('../db/models');
-const user = require('../db/models/user');
 
 const app = new Koa();
 const router = new Router();
@@ -13,11 +12,8 @@ app.use(bodyParser());
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.get('X-Response-Time');
+  // eslint-disable-next-line no-console
   console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
-
-router.get('/', (ctx, next) => {
-  ctx.body = 'what';
 });
 
 router.post('/users', async (ctx) => {
@@ -46,40 +42,37 @@ router.put('/users', async (ctx) => {
   };
   // I couldn't figure findorcreate out, ended up
   // with the brute approach
-  let user = await Models.User.findOne({
-    where: where,
-  });
+  let user = await Models.User.findOne({ where });
   // This may or may not be dangerous but if this is ever in prod
   // we should definetly make it safer to avoid injection
   if (!user) {
     user = await Models.User.create(newItem);
   } else {
-    await Models.User.update(newItem, { where: where });
+    await Models.User.update(newItem, { where });
     user = await Models.User.findOne({
-      where: where,
+      where,
     });
   }
   ctx.body = JSON.stringify(user);
 });
 
 router.get('/users/:id*', async (ctx) => {
-  const id = ctx.params.id;
+  const { id } = ctx.params;
   let where = {};
   if (id) {
     where = {
-      id: id,
+      id,
     };
   }
-  const response = await Models.User.findAll({ where: where });
+  const response = await Models.User.findAll({ where });
 
   ctx.body = JSON.stringify(response);
 });
 
 router.delete('/users/:id', async (ctx) => {
-  // Should I find the user?
-  const id = ctx.params.id;
-  let result = await Models.User.destroy({
-    where: { id: id },
+  const { id } = ctx.params;
+  const result = await Models.User.destroy({
+    where: { id },
   });
   ctx.body = JSON.stringify({ deleted: Boolean(result) });
 });
