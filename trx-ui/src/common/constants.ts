1d6560916d1d55bb65d1f9d9cd2d971f3c16c7ba
Diego Diaz <berith.py@gmail.com>
2020-10-17 12:06:08 -0300

    Cors fix?

--- trx-ui/src/common/constants.ts
+++ trx-ui/src/common/constants.ts
@@ -1,4 +1,4 @@
-// export const CORS_HELPER = '';
-export const CORS_HELPER = 'https://cors-anywhere.herokuapp.com/';
+// This is my own cors anywhere deployment because I was tired of being rate limited
+export const CORS_HELPER = 'https://corsanywhere-mskkgms6aq-uc.a.run.app/';
 export const BASE_URL = `${CORS_HELPER}https://www.metaweather.com/api/location/`;
 export const WEATHER_ICONS = ['sn', 'sl', 'h', 't', 'hr', 'lr', 's', 'hc', 'lc', 'c', 'rainbow'];
